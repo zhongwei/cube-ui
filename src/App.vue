676240288169a23828965a4ce85847488a4a94a9
Zhong Wei <zhongwei99@163.com>
2018-06-25 16:22:57 +0800

    Add bulma vue framework.

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  #app.container.is-fluid.is-marginless
+    #nav: .tabs.is-centered.is-boxed.is-medium
+      Tabs
+    router-view
+</template>
+
+<script>
+
+import Tabs from '@/components/Tabs.vue'
+
+export default {
+  name: 'home',
+
+  components: {
+    Tabs,
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  methods: {
+
+  },
+}
+</script>
+
+<style lang="scss">
+#app {
+  text-align: center;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  ul
+    li(v-for="(tab,index) in tabs"
+      :class="{'is-active': index == num}"
+      @click="selectedTab(index)")
+      router-link(:to="tab.url")
+        span.icon.is-small: i.fas(:class="tab.icon", aria-hidden='true')
+        span {{tab.name}}
+</template>
+
+<script>
+
+export default {
+  name: 'Tabs',
+
+  components: {
+  },
+
+  data() {
+    return {
+      tabs: [
+        { name: 'Home', icon: 'fa-home', url: '/' },
+        { name: 'Pictures', icon: 'fa-image', url: '/pic' },
+        { name: 'Music', icon: 'fa-music', url: '/music' },
+        { name: 'Videos', icon: 'fa-film', url: '/video' },
+        { name: 'Documents', icon: 'fa-file-alt', url: '/doc' },
+      ],
+      num: 0,
+    }
+  },
+
+  methods: {
+    selectedTab(index) {
+      this.num = index
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+</style>
